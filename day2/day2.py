11279269c7a609840cbb4950d70df3cd5dfce5c5
Coltor Leichtman <coltorleichtman@gmail.com>
2021-12-18 12:38:54 -0700

    day 2 complete

---
+++ day2/day2.py
@@ -0,0 +1,41 @@
+from typing import Tuple
+from enum import Enum
+import re
+
+
+class MoveAction(Enum):
+    FORWARD = 1
+    UP = 2
+    DOWN = 3
+
+
+def get_move_distance_pairs(inputs: list[str]) -> list[Tuple[MoveAction, int]]:
+    ret = []
+    regex = re.compile("^([fud].*) ([0-9]+)")
+
+    for el in inputs:
+        match = regex.match(el)
+
+        if match:
+            move_action = MoveAction[match.group(1).upper()]
+            distance = int(match.group(2))
+            ret.append((move_action, distance))
+
+    return ret
+
+
+def get_total_moved(moves: list[Tuple[MoveAction, int]]) -> Tuple[int, int]:
+    ret_hor = 0
+    ret_vert = 0
+
+    for action, distance in moves:
+        if action == MoveAction.UP:
+            ret_vert -= distance
+        elif action == MoveAction.DOWN:
+            ret_vert += distance
+        elif action == MoveAction.FORWARD:
+            ret_hor += distance
+        else:
+            raise Exception("A MoveAction can only be UP, DOWN, or FORWARD")
+
+    return ret_hor, ret_vert
---
+++ day2/day2_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+import day2
+
+
+def test_get_move_distance_pairs():
+    test_input = ["forward 23", "down 12", "up 777"]
+    test_output = day2.get_move_distance_pairs(test_input)
+
+    out_m, out_d = test_output[0]
+    assert out_m == day2.MoveAction.FORWARD and out_d == 23
+
+    out_m, out_d = test_output[1]
+    assert out_m == day2.MoveAction.DOWN and out_d == 12
+
+    out_m, out_d = test_output[2]
+    assert out_m == day2.MoveAction.UP and out_d == 777
+
+
+def test_get_total_moved():
+    with open("inputs1.txt") as f:
+        moves = day2.get_move_distance_pairs(f.readlines())
+        hor, vert = day2.get_total_moved(moves)
+        assert hor * vert == 150
+
+    with open("inputs2.txt") as f:
+        moves = day2.get_move_distance_pairs(f.readlines())
+        hor, vert = day2.get_total_moved(moves)
+        assert hor * vert == 1427868
